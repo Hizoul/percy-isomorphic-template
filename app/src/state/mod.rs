14939b75aea2fe850412c804fedf9850b3384530
MMB2 <git@mmb2.click>
2020-02-20 14:17:27 +0100

    :bug: fix test

--- app/src/state/mod.rs
+++ app/src/state/mod.rs
@@ -42,10 +42,10 @@ mod tests {
     use super::*;
     #[test]
     fn serialize_deserialize() {
-        let mut state = State::from_json(r#"{path":"/"}"#);
+        let mut state = State::from_json(r#"{"path":"/"}"#);
         assert_eq!(state.path(), "/");
         state.set_path("/test".to_owned());
         assert_eq!(state.path(), "/test");
-        assert_eq!(&state.to_json(), r#"{path":"/test"}"#);
+        assert_eq!(&state.to_json(), r#"{"path":"/test"}"#);
     }
 }
